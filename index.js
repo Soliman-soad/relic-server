11d4dc6087fc3fe398eb0a449abe906b0bd8d8f7
Soliman-soad <mdsolimansoad@gmail.com>
2022-11-27 18:01:34 +0600

    sellers delete secition added

--- index.js
+++ index.js
@@ -77,6 +77,15 @@ async function run(){
             res.send(result)
 
         })
+        app.delete('/sellers/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query ={
+                _id : ObjectId(id)
+            }
+            const result = await UserDatabase.deleteOne(query);
+            res.send(result)
+
+        })
         
     }catch(err){
         console.log(err.message);
